1f2fe68389ec7440179bbb6fd27609f3d42e7a23
Andrei Sciapaniuk <Andrei.Sciapaniuk@coxautoinc.com>
2024-05-23 21:52:58 +0400

    fix

--- WebDriverProject/Core/DriverFactory.cs
+++ WebDriverProject/Core/DriverFactory.cs
@@ -13,6 +13,7 @@ public class DriverFactory
     {
         var chromeOptions = new ChromeOptions();
         chromeOptions.AddArgument("--incognito");
+        chromeOptions.AddArgument("--remote-debugging-pipe");
 
         new DriverManager().SetUpDriver(new ChromeConfig());
         return new ChromeDriver(chromeOptions);
